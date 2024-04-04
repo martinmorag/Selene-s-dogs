71d137d5b6059b5856c8d6c4f3aa23bcca5c4dfa
Martin <moragamartin2@gmail.com>
2024-04-04 18:34:23 -0300

    about-me changes

--- src/js/Comments.mjs
+++ src/js/Comments.mjs
@@ -1,5 +1,20 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
+function formatDateTime(dateTime) {
+    if (!dateTime) return ''; // Handle empty date
+    const options = { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return new Date(dateTime).toLocaleDateString(undefined, options);
+}
+
+function theComment(comment) {
+    const item =   `<div class="comment">
+                        <span class="comment-time">${formatDateTime(comment.timestamp)}</span>
+                        <hr class="hr">
+                        <span class="comment-content">${comment.content}</span>
+                    </div>`
+    return item;
+}
+
 export default class Comments {
     constructor(key) {
         this.key = key;
@@ -7,6 +22,7 @@ export default class Comments {
     }
     async init() {
         this.getComment();
+        this.displayComments()
     }
     getComment() {
         const form = document.getElementById('CommentForm');
@@ -16,9 +32,20 @@ export default class Comments {
             this.sendComment(selectedValue); // Pass the comment value to sendComment()
         });
     }
-    sendComment(comment) {
+    sendComment(content) {
+        const timestamp = new Date().toISOString(); // Get current date and time
         let existingItems = getLocalStorage(this.key) || [];
-        existingItems.push(comment); // Use the passed comment value
+        existingItems.push({ content, timestamp }); // Store comment object with content and timestamp
         setLocalStorage(this.key, existingItems);
+        this.displayComments();
+    }
+    displayComments() {
+        let comments = getLocalStorage(this.key) || [];
+        const div = document.querySelector('.posted-comments');
+        div.innerHTML = '';
+
+        comments.forEach(comment => {
+            div.innerHTML += theComment(comment);
+        });
     }
 }
